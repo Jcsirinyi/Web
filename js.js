d143584e8ed8a5a5da95e7e920db74d54fef8247
Jcsirinyi <jcsirinyi@hotmail.com>
2025-08-13 14:37:22 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,1 @@
+function navigate(page) {if (page === 'home') {window.location.href = 'home.html';} else if (page === 'contact') {window.location.href = 'contact.html';} else if (page === 'about') {window.location.href = 'about.html';}}
